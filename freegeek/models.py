449eade80f60302b80172f4d447374548db94ec7
Michelle Brochmann <mbrochmann@gmail.com>
2017-07-29 10:37:04 -0700

    Issue #21: Improved docstrings.

--- freegeek/models.py
+++ freegeek/models.py
@@ -26,22 +26,24 @@ class Station(models.Model):
 
 class Appointment(models.Model):
     """Appointment model.
-    
-    Has a start_time and end_time.
-    Associated with a Station.
+
+    Attributes:
+       start_time (DateTimeField)
+       end_time (DateTimeField)
+       station (ForeignKey(Station))
 
     It needs "slots" which have proficiency requirements, 
     and each slot can be open or filled.
     (Do we need a "slot" model?)
-
-    The Appointment can be recast as a string which includes: 
-    Start and End time, station, building.
-    Should this string eventually include a summary of open and filled slots?
     """
     start_time = models.DateTimeField('start_time')
     end_time = models.DateTimeField('end_time')
     station = models.ForeignKey(Station, on_delete=models.CASCADE)
     def __str__(self):
+        """Recast Appointment as string which gives a summary of the Appointment.
+        This includes start_time, end_time, station, and location.
+        This should eventually include a summary of open and filled slots.
+        """
         appointment_string = ("Appointment: %s to %s at %s in %s" % 
                               (str(self.start_time), str(self.end_time), 
                                str(self.station), str(self.station.building)))
