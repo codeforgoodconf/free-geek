fea835d95386aeb40421da08b9fc5fbd7366f342
Rachel Kelly <rkellyalso@gmail.com>
2017-07-30 09:34:51 -0700

    adding a few wsgi items to hook up

--- freegeek/wsgi.py
+++ freegeek/wsgi.py
@@ -8,6 +8,20 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import sys
+import site
+
+# adding site-packages of desired virtualenv (in this case, py3.5)
+site.addsitedir('/mnt/freegeek/lib/python3.5/site-packages')
+
+# adding app's directory to the PYTHONPATH
+sys.path.append('/mnt/free-geek')
+sys.path.append('/mnt/free-geek/freegeek')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "freegeek.settings")
+
+# activate virtualenv
+activate_env = os.path.expanduser("/mnt/freegeek/bin/activate_this.py")
+exec(open(activate_env).read())
 
 from django.core.wsgi import get_wsgi_application
 
