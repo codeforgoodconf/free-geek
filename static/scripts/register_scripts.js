9e49f1f107532e73a2c97946045bb7fb8ca00fc5
Michelle Brochmann <mbrochmann@gmail.com>
2017-09-19 11:21:28 -0500

    Instant feedback for non-matching or too-short passwords.

--- static/scripts/register_scripts.js
+++ static/scripts/register_scripts.js
@@ -7,9 +7,16 @@ function checkBothPasswords() {
     //The first is to verify that the password falls within certain parameters
     var pWordOne = document.getElementById('password').value;
     var pWordTwo = document.getElementById('password_check').value;
-    if (pWordOne === pWordTwo && pWordOne.length >= 5) {
-        document.getElementById('register_button').disabled = false;
-    } else {
-        document.getElementById('register_button').disabled = true;
+
+    document.getElementById('register_button').disabled = false;
+    document.getElementById('password_error').innerHTML='';
+    if (pWordOne !== pWordTwo) {
+	document.getElementById('register_button').disabled = true;
+	document.getElementById('password_error').innerHTML += 'Passwords must be equal.<br>';
     }
+    if (pWordOne.length < 5) {
+	document.getElementById('register_button').disabled = true;
+	document.getElementById('password_error').innerHTML +=('Password must be at least 5 characters.<br>');
+    }
+
 }
