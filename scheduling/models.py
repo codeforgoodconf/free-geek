5991166ca7794d4ef7e3acf922a8713996a91c03
Kenneth Black <kenneth.black@live.com>
2018-01-14 17:56:47 -0800

    Add dependent models for Volunteer models

--- scheduling/models.py
+++ scheduling/models.py
@@ -20,16 +20,16 @@ class VolunteerTaskType(models.Model):
 
 
 class VolunteerTask(models.Model):
-    contact_id = models.IntegerField(blank=True, null=True)
     duration = models.FloatField(blank=True, null=True)
     lock_version = models.IntegerField()
     updated_at = models.DateTimeField(blank=True, null=True)
     created_at = models.DateTimeField(blank=True, null=True)
     date_performed = models.DateField(blank=True, null=True)
 
-    community_service_type_id = models.IntegerField(blank=True, null=True)
+    contact = models.ForeignKey(Contact, on_delete=models.DO_NOTHING)
     program = models.ForeignKey(Program, on_delete=models.DO_NOTHING)
     volunteer_task_type = models.ForeignKey(VolunteerTaskType, on_delete=models.DO_NOTHING)
+    community_service_type = models.ForeignKey('CommunityServiceType', blank=True, null=True, on_delete=models.DO_NOTHING)
     updated_by = models.ForeignKey(User, related_name='updated_by_user', on_delete=models.DO_NOTHING)
     created_by = models.ForeignKey(User, related_name='created_by_user', on_delete=models.DO_NOTHING)
 
@@ -39,9 +39,10 @@ class VolunteerTask(models.Model):
 
 class VolunteerDefaultEvent(models.Model):
     week = models.CharField(max_length=1, blank=True, null=True)
-    weekday_id = models.IntegerField()
     description = models.CharField(max_length=255, blank=True, null=True)
     notes = models.TextField(blank=True, null=True)
+
+    weekday = models.ForeignKey('Weekday', on_delete=models.DO_NOTHING)
 
     class Meta:
         db_table = 'volunteer_default_events'
@@ -112,11 +113,48 @@ class Assignment(models.Model):
     created_at = models.DateTimeField(blank=True, null=True, auto_now_add=True)
     updated_at = models.DateTimeField(blank=True, null=True, auto_now=True)
 
-    call_status_type_id = models.IntegerField(blank=True, null=True)
-    attendance_type_id = models.IntegerField(blank=True, null=True)
-
     contact = models.ForeignKey(Contact, on_delete=models.DO_NOTHING)
     volunteer_shift = models.ForeignKey(VolunteerShift, on_delete=models.DO_NOTHING)
+    call_status_type = models.ForeignKey('CallStatusType', blank=True, null=True, on_delete=models.DO_NOTHING)
+    attendance_type = models.ForeignKey('AttendanceType', blank=True, null=True, on_delete=models.DO_NOTHING)
 
     class Meta:
         db_table = 'assignments'
+
+
+class AttendanceType(models.Model):
+    name = models.CharField(max_length=255, blank=True, null=True)
+    cancelled = models.NullBooleanField()
+
+    class Meta:
+        db_table = 'attendance_types'
+
+
+class CallStatusType(models.Model):
+    name = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        db_table = 'call_status_types'
+
+
+class Weekday(models.Model):
+    name = models.CharField(max_length=255, blank=True, null=True)
+    short_name = models.CharField(max_length=255, blank=True, null=True)
+    is_open = models.NullBooleanField()
+    start_time = models.TimeField(blank=True, null=True)
+    end_time = models.TimeField(blank=True, null=True)
+    open_time = models.TimeField()
+    close_time = models.TimeField()
+
+    class Meta:
+        db_table = 'weekdays'
+
+
+class CommunityServiceType(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.CharField(max_length=100, blank=True, null=True)
+    hours_multiplier = models.FloatField()
+    lock_version = models.IntegerField()
+
+    class Meta:
+        db_table = 'community_service_types'
