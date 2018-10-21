64ff461df5b8a8c36bee8380b8ace7c1d6499829
Kenneth Black <kenneth.black@live.com>
2018-10-20 17:29:08 -0700

    Update convert_json_to_fixture.js

--- utils/convert_json_to_fixture.js
+++ utils/convert_json_to_fixture.js
@@ -7,8 +7,7 @@ var details = parsed_json.map((row) => {
   dingo.model = "scheduling.VolunteerDefaultShift"
   dingo.fields = {}
   let keys = Object.keys(row)
-
-  // drop id
+  
   keys.filter(key => key !== 'id')
     .map((key) => {
       dingo.fields[key] = row[key]
@@ -16,21 +15,6 @@ var details = parsed_json.map((row) => {
 
   return dingo
 })
-console.log(details)
 
 const str = JSON.stringify(details)
 fs.writeFile('volunteer_default_shifts.json', str, 'utf8', ()=> {});
-
-// 1. convert csv to json then
-  // csvtojson volunteer_default_shifts.csv > converted_volunteer_default_shifts.json
-// 2. use custom js script to take parsed json grab correct fields and change shape
-  // node convert_json_to_fixture_volunteer_default_shifts.js
-// 3. beautify json in editor to verify json shape
-// 4.
-  // mv converted_volunteer_default_shifts.json ~/code/freegeek/free-geek/scheduling/fixtures/
-//
-
-
-
-
-// TODO: get extender cable back for laptop
